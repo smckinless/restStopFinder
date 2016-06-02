744473506181113a1ed46ebc4bc4364f2568e1ee
smckinless <smckinless@gmail.com>
2016-06-02 15:50:55 -0400

    Add directions capabilities and new design

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ YOUR_API_KEY = "AIzaSyAveMfGooZzTbOq5rVO-OeyyUbVnn6Nc3g"
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    return render_template('index.html')
 
-	return render_template('index.html')
+@app.route("/restStopFinder", methods=["GET", "POST"])
+def finder():
+
+	return render_template('finder.html')
 
 @app.route('/result', methods=["GET", "POST"])
 def find_stop():
@@ -19,8 +23,10 @@ def find_stop():
     #geocodedLocation = json.load(urllib2.urlopen("https://maps.googleapis.com/maps/api/geocode/json?latlng="+str(lat)+","+str(lon)+"&key="+YOUR_API_KEY))
     #address = geocodedLocation['results'][0]['formatted_address']
     stop = findPosition(lat, lon)
+    
     #stopLocation = json.load(urllib2.urlopen("https://maps.googleapis.com/maps/api/geocode/json?latlng="+str(stop.location[1])+","+str(stop.location[0])+"&key="+YOUR_API_KEY))
     #stopAddress = stopLocation['results'][0]['formatted_address']
+    #direction_string = '"?key="'+YOUR_API_KEY+'&origin='+address+'&destination='+stopAddress+'"'
     directions = json.load(urllib2.urlopen("https://maps.googleapis.com/maps/api/directions/json?origin="+str(lat)+","+str(lon)+"&destination="+str(stop.location[1])+","+str(stop.location[0])+"&key="))
     #for direction in directions:
     steps = directions['routes'][0]['legs'][0]['steps']
@@ -28,8 +34,9 @@ def find_stop():
     instructions = stop.name + "<br><br>You are " + total_distance + " from your destination.<br><br>"
     for step in steps:
         instructions += step['html_instructions'] + " for " + step['distance']['text'] + "." + "<br>"
-
-    return instructions
+    return '<iframe width="100%" height="100%" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/directions?origin='+str(lat)+','+str(lon)+'&destination='+str(stop.location[1])+','+str(stop.location[0])+'&key='+YOUR_API_KEY+'" allowfullscreen></iframe>'
+    #return "<a href=https://www.google.com/maps/embed/v1/directions?key="+YOUR_API_KEY+"&origin="+str(lat)+","+str(lon)+"&destination="+str(stop.location[1])+","+str(stop.location[0])+">Link</a>"
+    #return instructions
     #render_template("result.html", steps=steps)
     #print stopAddress
     #return render_template("result.html", text=text)
