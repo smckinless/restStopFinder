f1889d335df44935aa825b25bc3de113d0044c2c
smckinless <smckinless@gmail.com>
2016-08-05 13:00:15 -0400

    direction based functionality

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from parser import *
 import os, urllib2
 
 app = Flask(__name__)
-YOUR_API_KEY = "AIzaSyAveMfGooZzTbOq5rVO-OeyyUbVnn6Nc3g"
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -16,7 +15,6 @@ def finder():
 
 @app.route('/result', methods=["GET", "POST"])
 def find_stop():
-    # print request.json
     beginLat = float(request.json['beginLat'])
     beginLon = float(request.json['beginLon'])
     endLat = float(request.json['endLat'])
@@ -24,24 +22,23 @@ def find_stop():
     northBound = False
     eastBound = False
     
-
+    # checks if heading northbound
     if (endLat > beginLat):
         northBound = True
+
+    # checks if heading eastbound
     if (endLon < beginLon):
         eastBound = True
     stop = findPosition(endLat, endLon, northBound, eastBound)
     
+    # if no stop in current direction, check again
     if stop is False:
         stop = findPosition(endLat, endLon, northBound, eastBound)
-    directions = json.load(urllib2.urlopen("https://maps.googleapis.com/maps/api/directions/json?origin="+str(endLat)+","+str(endLon)+"&destination="+str(stop.location[1])+","+str(stop.location[0])+"&key="))
-    steps = directions['routes'][0]['legs'][0]['steps']
-    total_distance = directions['routes'][0]['legs'][0]['distance']['text']
-    instructions = stop.name + "<br><br>You are " + total_distance + " from your destination.<br><br>"
-    for step in steps:
-        instructions += step['html_instructions'] + " for " + step['distance']['text'] + "." + "<br>"
+
+    # find directions from current position to stop via Google
     return '<iframe width="100%" height="100%" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/directions?origin='+str(endLat)+','+str(endLon)+'&destination='+str(stop.location[1])+','+str(stop.location[0])+'&key='+YOUR_API_KEY+'" allowfullscreen></iframe>'
 
 
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
-	app.run(debug=False, host='0.0.0.0', port=port)
+	app.run(debug=True, host='0.0.0.0', port=port)
--- parser.py
+++ parser.py
@@ -4,12 +4,11 @@ from urllib2 import urlopen
 from contextlib import closing
 import json
 
+# finds closest rest stop to current position based on direction
 def findPosition(lat, lon, northBound, eastBound):
-
-
 	restStops = []
 
-
+	# creates a list of rest stops in the country
 	with open('RestAreas.csv', 'rb') as csvfile:
 		reader = csv.reader(csvfile)
 		for row in reader:
@@ -20,6 +19,8 @@ def findPosition(lat, lon, northBound, eastBound):
 
 	dist = 1000
 	correctStop = False
+
+
 	for stop in restStops:
 		#print stop.location
 		stopLong = float(stop.location[0])
@@ -27,25 +28,39 @@ def findPosition(lat, lon, northBound, eastBound):
 		latDistance = abs(stopLat - lat)
 		longDistance = abs(stopLong - lon)
 		totalDistance = math.sqrt((latDistance * latDistance) + (longDistance * longDistance))
+
+		# checks if moving northeast
 		if northBound is True and eastBound is True:
 			if totalDistance < dist and stopLat > lat and stopLong > lon:
 				dist = totalDistance
-				correctStop = stop
+				foundStop = stop
+				correctStop = True
+
+		# checks if moving east and south
 		elif northBound is False and eastBound is True:
 			if totalDistance < dist and stopLat < lat and stopLong > lon:
 				dist = totalDistance
-				correctStop = stop
+				foundStop = stop
+				correctStop = True
+
+		# checks if moving north and west
 		elif northBound is True and eastBound is False:
 			if totalDistance < dist and stopLat > lat and stopLong < lon:
 				dist = totalDistance
-				correctStop = stop
+				foundStop = stop
+				correctStop = True
+
+		# moving south and west
 		else:
 			if totalDistance < dist and stopLat < lat and stopLong < lon:
 				dist = totalDistance
-				correctStop = stop
+				foundStop = stop
+				correctStop = True
+
 
 	dist = 1000
 
+	# if no stop in current direction find nearest stop in all directions
 	if correctStop is False:
 		for stop in restStops:
 		#print stop.location
